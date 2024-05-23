6388af85136bf4bd6289917ab308cd1f13acf5ba
dadadave80 <daveproxy80@gmail.com>
2024-05-23 21:15:25 +0100

    feat: add foundry

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,9 @@
 /* global ethers task */
 require('@nomiclabs/hardhat-waffle')
 
+/* foundry */
+require("@nomicfoundation/hardhat-foundry");
+
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task('accounts', 'Prints the list of accounts', async () => {
